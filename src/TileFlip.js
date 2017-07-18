28c4f68cd3314a218ef7b8e2e97a17faebce0055
Chijioke Okafo <cokafo@innocean.ca>
2017-07-18 10:58:14 -0400

    Updating TileFlip.js to work with react component lifecycle, removing jQuery document.ready

--- src/TileFlip.js
+++ src/TileFlip.js
@@ -3,20 +3,20 @@ import './TileFlip.css';
 import $ from 'jquery';
 
 class TileFlip extends Component {
-  render() {
-    $( document ).ready(function() {
-      $('.tile-flip').each(function(){
-        var flipContainer = $(this);
-        $('.flip-container', flipContainer).each(function(){
-          $(this).click(function(){
-            var wasOpen = $('.flipper', this).hasClass('flip');
-            $('.flipper', flipContainer).removeClass('flip');
-            if(!wasOpen) $('.flipper', this).addClass('flip');
-          });
+  componentDidMount() {
+    $('.tile-flip').each(function(){
+      var flipContainer = $(this);
+      $('.flip-container', flipContainer).each(function(){
+        $(this).click(function(){
+          var wasOpen = $('.flipper', this).hasClass('flip');
+          $('.flipper', flipContainer).removeClass('flip');
+          if(!wasOpen) $('.flipper', this).addClass('flip');
         });
       });
-    });  
+    });
+  }
 
+  render() {  
     return (
       <div className="TileFlip">
         <div className="tile-flip desktop">
@@ -26,7 +26,7 @@ class TileFlip extends Component {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
               </div>
               <div className="back">
-                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                <p>Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an 
                 unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, 
                 remaining essentially unchanged.</p>
@@ -39,7 +39,7 @@ class TileFlip extends Component {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
               </div>
               <div className="back">
-                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                <p>Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an 
                 unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, 
                 remaining essentially unchanged.</p>
@@ -52,7 +52,7 @@ class TileFlip extends Component {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
               </div>
               <div className="back">
-                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                <p>Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an 
                 unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, 
                 remaining essentially unchanged.</p>
@@ -65,7 +65,7 @@ class TileFlip extends Component {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
               </div>
               <div className="back">
-                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                <p>Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an 
                 unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, 
                 remaining essentially unchanged.</p>
